6171655d75cc9365a5a15b4a94f40450a187d7bf
Guy David <guyda96@gmail.com>
2025-02-11 14:15:02 +0200

    [AArch64] Skip storing of stack arguments when lowering tail calls
    When possible, do not emit trivial load and stores to the same offset on
    the stack.

--- llvm/include/llvm/CodeGen/SelectionDAGNodes.h
+++ llvm/include/llvm/CodeGen/SelectionDAGNodes.h
@@ -722,6 +722,19 @@ public:
     }
   }
 
+  /// Test if this node is an assert operation.
+  bool isAssertOpcode() const {
+    switch (NodeType) {
+    default:
+      return false;
+    case ISD::AssertAlign:
+    case ISD::AssertNoFPClass:
+    case ISD::AssertSext:
+    case ISD::AssertZext:
+      return true;
+    }
+  }
+
   /// Test if this node is a vector predication operation.
   bool isVPOpcode() const { return ISD::isVPOpcode(getOpcode()); }
 
